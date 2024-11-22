be8645ec79b1a0393bb754010eec490076aa28da
Andrea de Martinis <andreademartinis86@gmail.com>
2024-11-22 23:03:16 +0100

    Bug Fix: var nationalID in updateGuest

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -10,7 +10,10 @@ export async function updateGuest(formData) {
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
-  const nationalID = formData.get("nationalID") || "";
+  const nationalID =
+    formData.get("nationalID") != false
+      ? formData.get("nationalID")
+      : "1234567";
   const [nationality, countryFlag] = formData.get("nationality").split("%");
 
   if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
